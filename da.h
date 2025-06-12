d496c8a7ecb8a173f982095d95df3540c3b61d2e
kurth4cker <kurth4cker@gmail.com>
2025-06-12 20:50:40 +0300

    da: add da_declare_append() macro

--- da.h
+++ da.h
@@ -15,6 +15,9 @@
         Type *data;                            \
     } Array_Type
 
+#define da_declare_append(Array_Type, Type, Name) \
+    bool da_append_##Name(Array_Type *arr, Type val)
+
 // Pre defined array types. Order of values should be same. size, capacity and
 // data. Names does not matter.
 da_define_struct(Da_Int, int);
@@ -23,23 +26,22 @@ da_define_struct(Da_Size, size_t);
 da_define_struct(Da_Str, const char *);
 
 // append given value to the end of array, initializes if necessary, grows if necessary.
-bool da_append_int(Da_Int *arr, int val);
-bool da_append_double(Da_Double *arr, double val);
-bool da_append_size(Da_Size *arr, size_t val);
-bool da_append_str(Da_Str *arr, const char *val);
+// bool da_append_int(Da_Int *arr, int val);
+da_declare_append(Da_Int, int, int);
+da_declare_append(Da_Double, double, double);
+da_declare_append(Da_Size, size_t, size);
+da_declare_append(Da_Str, const char *, str);
 
 
 // You can create your own dynamic arrays with this helper functions.
 // See da.c for example implementations. You can also take a look at example.c.
 #define da_init_if_needed(arr)    da_generic_init_if_needed((Da_Generic *)(arr), sizeof(*(arr)->data))
 #define da_grow_if_needed(arr)    da_generic_grow_if_needed((Da_Generic *)(arr), sizeof(*(arr)->data))
-// #define da_destroy(arr)
-//     do {
-//         free((arr)->data);
-//         (arr)->capacity = 0;
-//     } while (0)
-
-#define da_destroy(arr)           da_generic_destroy((Da_Generic *)(arr))
+#define da_destroy(arr)          \
+    do {                         \
+        free((arr)->data);       \
+        (arr)->capacity = 0;     \
+    } while (0)
 
 da_define_struct(Da_Generic, void);
 
